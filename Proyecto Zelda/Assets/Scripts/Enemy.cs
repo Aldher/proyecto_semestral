813394bbb1f4e0e060db153a9d568246e926f302
begines <36993958+begines@users.noreply.github.com>
2018-12-01 17:17:44 -0600

    Otro Ejercicio
    Por segunda vez realice lo del anterior ejercicio gracias a aldair que el tonto borro lo que hice

---
+++ Proyecto Zelda/Assets/Scripts/Enemy.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    // Variables para gestionar el radio de visión, el de ataque y la velocidad 
+    public float visionRadius;
+    public float attackRadius;
+    public float speed;
+
+    // Variables relacionadas con el ataque
+    [Tooltip("Prefab de la roca que se disparará")]
+    public GameObject Baluqui;
+    [Tooltip("Velocidad de ataque (segundos entre ataques)")]
+    public float attackSpeed = 2f;
+    bool attacking;
+
+    ///--- Variables relacionadas con la vida
+    [Tooltip("Puntos de vida")]
+    public int maxHp = 3;
+    [Tooltip("Vida actual")]
+    public int hp;
+
+    // Variable para guardar al jugador
+    GameObject player;
+
+    // Variable para guardar la posición inicial
+    Vector3 initialPosition, target;
+
+    // Animador y cuerpo cinemático con la rotación en Z congelada
+   
+    Rigidbody2D rb2d;
+
+    void Start () {
+
+        // Recuperamos al jugador gracias al Tag
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // Guardamos nuestra posición inicial
+        initialPosition = transform.position;
+        
+        rb2d = GetComponent<Rigidbody2D>();
+
+        ///--- Iniciamos la vida
+        hp = maxHp;
+    }
+
+    void Update () {
+
+        // Por defecto nuestro target siempre será nuestra posición inicial
+        target = initialPosition;
+
+        // Comprobamos un Raycast del enemigo hasta el jugador
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position, 
+            player.transform.position - transform.position, 
+            visionRadius, 
+            1 << LayerMask.NameToLayer("Default") 
+              
+        );
+
+        // Aquí podemos debugear el Raycast
+        Vector3 forward = transform.TransformDirection(player.transform.position - transform.position);
+        Debug.DrawRay(transform.position, forward, Color.red);
+
+        // Si el Raycast encuentra al jugador lo ponemos de target
+        if (hit.collider != null) {
+            if (hit.collider.tag == "Player"){
+                target = player.transform.position;
+            }
+        }
+
+        // Calculamos la distancia y dirección actual hasta el target
+        float distance = Vector3.Distance(target, transform.position);
+        Vector3 dir = (target - transform.position).normalized;
+
+        // Si es el enemigo y está en rango de ataque nos paramos y le atacamos
+        if (target != initialPosition && distance < attackRadius){
+       
+            ///-- Empezamos a atacar 
+            if (!attacking) StartCoroutine(Attack(attackSpeed));
+        }
+        // En caso contrario nos movemos hacia él
+        else {
+            rb2d.MovePosition(transform.position + dir * speed * Time.deltaTime);
+
+        }
+
+        // Una última comprobación para evitar bugs forzando la posición inicial
+        if (target == initialPosition && distance < 0.05f){
+            transform.position = initialPosition; 
+         
+        }
+
+        // Y un debug optativo con una línea hasta el target
+        Debug.DrawLine(transform.position, target, Color.green);
+    }
+
+    // Podemos dibujar el radio de visión y ataque sobre la escena dibujando una esfera
+    void OnDrawGizmosSelected() {
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+    }
+
+    IEnumerator Attack(float seconds){
+        attacking = true;  // Activamos la bandera
+        // Si tenemos objetivo y el prefab es correcto creamos la roca
+        if (target != initialPosition && Baluqui != null) {
+            Instantiate(Baluqui, transform.position, transform.rotation);
+            // Esperamos los segundos de turno antes de hacer otro ataque
+            yield return new WaitForSeconds(seconds);
+        }
+        attacking = false; // Desactivamos la bandera
+    }
+
+    ///--- Gestión del ataque, restamos una vida
+    public void Attacked(){
+        if (--hp <= 0) Destroy(gameObject);
+    }
+
+ 
+
+}
